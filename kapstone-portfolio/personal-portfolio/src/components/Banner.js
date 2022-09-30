56d9a3b6c1a349848660d8febeb10a3f48b2fa17
NIGHTWINGBIH <dionte7481@gmail.com>
2022-09-30 00:07:03 -0500

    first commit

---
+++ kapstone-portfolio/personal-portfolio/src/components/Banner.js
@@ -0,0 +1,16 @@
+
+import { Container, Row } from "react-bootstrap";
+
+
+export const Banner = () => {
+  
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <h3>Welcome To My Portfolio</h3>
+        </Row>
+      </Container>
+    </section>
+  )
+}
